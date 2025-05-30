569e4bc8d352c32d43378679fc88b82daf78bf6d
AlephNought0 <alephnought0@proton.me>
2025-05-30 19:13:07 +0200

    Create folders and cmake file

---
+++ flake.nix
@@ -0,0 +1,34 @@
+# flake.nix (in your repo root)
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs.buildPackages; [
+        cmake
+        ninja
+        pkg-config
+      ];
+
+      buildInputs = with pkgs.buildPackages; [
+        vulkan-headers
+        vulkan-loader
+        vulkan-tools
+        vulkan-validation-layers
+        glfw
+        openal
+        assimp
+        entt
+        glm
+      ];
+
+      LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
+      VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+    };
+  };
+}
