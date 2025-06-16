22dd9220f0589e283fddc8f325b44feab1676ee2
AlephNought0 <alephnought0@proton.me>
2025-06-16 13:12:56 +0200

    Replace useless assertions with runtime_error

--- core/vulkan/vulkan_types.cpp
+++ core/vulkan/vulkan_types.cpp
@@ -1,11 +1,19 @@
 #include "vulkan_types.hpp"
 
 #include <cassert>
+#include <stdexcept>
 #include <vulkan/vulkan.h>
 
 static vulkan_context s_context;
 static vulkan_device s_device;
 static bool initialized = false;
+
+void checkInit() {
+  if (!initialized) {
+    throw std::runtime_error("Vulkan structures were not initialized. Call "
+                             "initializeVkStructs() first.");
+  }
+}
 
 void initializeVkStructs() {
   if (initialized)
@@ -16,13 +24,13 @@ void initializeVkStructs() {
 }
 
 vulkan_context &getVulkanContextStruct() {
-  assert(initialized && "Vulkan structures were not initialized. Call "
-                        "initializeVkStructs() first.");
+  checkInit();
+
   return s_context;
 }
 
 vulkan_device &getVulkanDeviceStruct() {
-  assert(initialized && "Vulkan structures were not initialized. Call "
-                        "initializeVkStructs() first.");
+  checkInit();
+
   return s_device;
 }
